a837d4c62a62986a4957401797d39e38d6414e27
abiodevc <abiocesar1@gmail.com>
2023-01-21 12:48:10 -0300

    fiz algumas modificações fúteis

--- guess_the_number_game/guess_the_number.py
+++ guess_the_number_game/guess_the_number.py
@@ -1,27 +1,25 @@
 from random import randint
 
 def main():
-    print('''|---------------------------------|
-|jogo de adivinhar o numero blabla|
-|---------------------------------|''')
-    print('digite seu chute:')
+    name = input('Qual o seu nome? ')
+    print(f'Ola, {name}, eu estou pensando em um numero entre 1 e 100, tente adivinhar!')
 
-    num = randint(0, 99)
+    num = randint(1, 100)
     gameon = True
     tries = 0
 
     while gameon:
         tries += 1
-        player = int(input(''))
+        player = int(input('seu palpite: '))
         if player == num:
             print('parabens!! voce acertou.')
             print(f'numero de tentativas: {tries}')
             gameon = False
         elif player > num:
-            print('voce errou, chutou um numero muito alto.')
-            print('tente novamente!!')
+            print('seu palpite foi muito alto!')
+            print('tente novamente.')
         elif player < num:
-            print('voce errou, chutou um numero muito baixo.')
-            print('tente novamente!!')
+            print('seu palpite foi muito baixo!')
+            print('tente novamente.')
 
 main()
